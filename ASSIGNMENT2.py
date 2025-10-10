064c44594b15d0042141acc122963c50c47aa3d5
MOHAMMED FAZLUR RAHMAN <mr4676@g.rit.edu>
2025-10-10 19:32:31 +0400

    Update ASSIGNMENT2.py

--- ASSIGNMENT2.py
+++ ASSIGNMENT2.py
@@ -1,11 +1,11 @@
 def check_limit(borrowed):
     """This function takes the argument borrowed, which refers to the number of books
     borrowed and checks whether the number of books borrowed are within or over limit"""
-    if borrowed <= 3:
+    if borrowed < 0:
+        return "Error: Invalid number of books"
+    elif borrowed <= 3:
         return "Within limit"
-    elif borrowed > 3 and borrowed <= 6:
+    elif borrowed <= 6:
         return "Over limit: Fine $5"
-    elif borrowed > 6:
+    else:
         return "Over limit: Fine $10"
-    elif borrowed < 0:
-        return "Error: invalid number of books"
