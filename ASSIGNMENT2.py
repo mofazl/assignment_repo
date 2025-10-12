d33a1d5f4b93f47e05a9bdff49d8ecd4b611c366
MOHAMMED FAZLUR RAHMAN <mr4676@g.rit.edu>
2025-10-12 16:41:33 +0400

    Update ASSIGNMENT2.py

--- ASSIGNMENT2.py
+++ ASSIGNMENT2.py
@@ -1,11 +1,38 @@
+#STUDENT1: MOHAMMED FAZLUR RAHMAN
+#STUDENTID: 426001122
+#STUDENT2:
+#STUDENTID:
+#STUDENT3:
+#STUDENTID:
 def check_limit(borrowed):
-    """This function takes the argument borrowed, which refers to the number of books
-    borrowed and checks whether the number of books borrowed are within or over limit"""
+    """
+    TASK 1:
+    This function takes the argument (borrowed), which refers to the number of books
+    borrowed and checks whether the number of books borrowed are within or over limit
+    """
+    borrowed = int(borrowed) #Converts borrowed into an integer to allow numeric comparisons
     if borrowed < 0:
-        return "Error: Invalid number of books"
+        return "Error: Invalid number of books" #Uses an if conditional to check if the number of books borrowed is less than 0, and if it is then returns a string stating that the number of books borrowed is invalid
     elif borrowed <= 3:
-        return "Within limit"
+        return "Within limit" #Uses an elif conditional to check whether the number of books is within limit 
     elif borrowed <= 6:
-        return "Over limit: Fine $5"
+        return "Over limit: Fine $5" #Uses an elif conditional to check whether the number of books is over limit and the fine is $5
     else:
-        return "Over limit: Fine $10"
+        return "Over limit: Fine $10" #Uses an else conditional that is used when the number of books borrowed is greater than 6 and returns a string stating that the number of books borrowed is over limit and the fine is $10
+def process_borrowers(filename):
+    """
+    TASK2:
+    This function takes an argument (filename), then it opens the given file in read mode and prints the students name and their status.
+    It also calls the previous function check_limit to check the status of the given student and uses try and except to check if theres
+    invalid inputs in the BooksBorrowed column
+    """
+    with open(filename,"r") as file: #Uses a with as statement to open the file in read mode. with as is used as it automatically closes the file
+        next(file) #Skips the header of the csv file
+        for line in file:
+            line = line.strip().split(",") #Strips the whitespace and splits the comma separated values into separate tokens
+            try:
+                name = line[0]
+                status = check_limit(line[1]) #check_limit function is called to check the status
+                print(name, status)
+            except ValueError:
+                print("Error: Non-numeric value for",line[0]) #Uses try and except to print the name and status of each entry but if there is an invalid input (number of books borrowed is non-numeric) then it prints out the students name and an error message stating there is a non numeric value associated to their entry
